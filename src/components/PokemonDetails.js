ff545cb7f7b39f82b91cfd188b7b17cb6b74d361
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-24 20:44:58 +0100

    in the PokemonDetails component set the states for the data, loading message and error message; use useEffect hook to fetch specific pokemon data from pokeApi by id; within the fetch function adjust the states of the loading and error messages and display them accordingly in the JSX template

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,6 +1,39 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Page } from "../components/page";
+import { Title } from "./title";
+
 const PokemonDetails = () => {
-    return ( 
-        null
+    const { id } = useParams();
+    const [pokemonData, setPokemonData] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(res => {
+                // console.log(res.data)
+                setPokemonData(res.data);
+                setIsLoading(false);
+                setError(false);
+            })
+            .catch(err => {
+                console.log(err);
+                setError(true);
+                setErrorMessage('Pokemon not found!');
+                setIsLoading(false)
+            })
+    }, [id]);
+
+    return (
+        <Page>
+            <div className="pokemon-details-wrapper text-white poke-font">
+                { error ? (<p className="poke-font py-6 text-center">{ errorMessage }</p>) : null }
+                { isLoading && <p className="text-white poke-font py-6 text-center">Loading...</p> }
+            </div>
+        </Page> 
      );
 }
  
