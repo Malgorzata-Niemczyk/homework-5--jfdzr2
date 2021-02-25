2f073995cd0264f04ae9a931d6b0d0f74a28f690
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-25 21:44:31 +0100

    adjust the CSS styles of the buttons in the PaginationButtons component

--- src/components/PaginationButtons.js
+++ src/components/PaginationButtons.js
@@ -1,10 +1,10 @@
 const PaginationButtons = () => {
     return ( 
-        <div className="pagination-buttons-wrapper flex">
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+        <div className="pagination-buttons-wrapper flex poke-font">
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
             Prev
           </button>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
             Next
           </button>
         </div>
