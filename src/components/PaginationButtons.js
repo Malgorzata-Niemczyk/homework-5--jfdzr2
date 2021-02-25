cb88bd6debce5d97752b0ba5128336b91db19d0b
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-25 22:48:27 +0100

    add the click events to the button and pass the functions in them from the props: when tested however, the functions do not work at all

--- src/components/PaginationButtons.js
+++ src/components/PaginationButtons.js
@@ -1,10 +1,10 @@
 const PaginationButtons = ({handleGoToNextPage, handleGoToPreviousPage}) => {
     return ( 
         <div className="pagination-buttons-wrapper flex poke-font">
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={handleGoToPreviousPage}>
             Prev
           </button>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={handleGoToNextPage}>
             Next
           </button>
         </div>
