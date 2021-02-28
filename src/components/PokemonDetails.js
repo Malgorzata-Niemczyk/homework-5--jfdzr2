07f40224061561005245160dd43760c182468070
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-28 20:42:00 +0100

    apply the custom hook useLocalStorage to store favourite pokemons in localStorage and create the handleAddToFavPokemons function that adds an object with the pokemon's data to localStorage upon clicking the add to fav button

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -4,6 +4,7 @@ import { useParams, useHistory } from "react-router-dom";
 import { Page } from "./page";
 import { Title } from "./title";
 import { pokemonTypesColors } from "../components/pokemonTypesColors";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const PokemonDetails = () => {
     const { index } = useParams();
@@ -12,6 +13,31 @@ const PokemonDetails = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
+    const [favourites, setFavourites] = useLocalStorage('favourite-pokemons', [
+        {
+            id: 1,
+            name: "bulbasaur",
+            avatar:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png",
+            types: [
+                {
+                slot: 1,
+                type: {
+                    name: "grass",
+                    url: "https://pokeapi.co/api/v2/type/12/",
+                },
+                },
+                {
+                slot: 2,
+                type: {
+                    name: "poison",
+                    url: "https://pokeapi.co/api/v2/type/4/",
+                },
+                },
+            ], 
+        }
+    ]);
+
 
     useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${index}`)
@@ -29,6 +55,17 @@ const PokemonDetails = () => {
             })
     }, [index]);
 
+    const handleAddToFavPokemons = () => {
+        let favPokemonItem = {
+            id: pokemonData.id,
+            name: pokemonData.name,
+            avatar: pokemonData.sprites.front_default,
+            types: pokemonData.types
+        };
+
+        setFavourites([...favourites, favPokemonItem]);
+    }
+
     return (
         <Page>
             <button className="poke-font bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" style={{textTransform: "uppercase"}} onClick={() => history.push('/pokemons')}>
@@ -41,7 +78,11 @@ const PokemonDetails = () => {
                     <div className="pokemon-details-card bg-red-700">
                         <div className="pokemon-details-title">
                             <Title>#{pokemonData.id}: {pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</Title>
-                            <button className="poke-font bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" style={{textTransform: "uppercase"}}>
+                            <button 
+                                className="poke-font bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" 
+                                style={{textTransform: "uppercase"}}
+                                onClick={handleAddToFavPokemons}
+                            >
                                 Add To ❤️
                             </button>
                         </div>
