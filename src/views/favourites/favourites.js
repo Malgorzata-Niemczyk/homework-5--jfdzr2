d17be6424143f38388d1f6831cd7bfe4aa41a369
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-28 18:37:07 +0100

    include the task instructions in the comment brackets

--- src/views/favourites/favourites.js
+++ src/views/favourites/favourites.js
@@ -3,35 +3,35 @@ import { Page } from '../../components/page'
 import { Title } from '../../components/title'
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 
-const examplePokemon = {
-  id: 1,
-  name: "bulbasaur",
-  avatar:
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png",
-  types: [
-    {
-      slot: 1,
-      type: {
-        name: "grass",
-        url: "https://pokeapi.co/api/v2/type/12/",
-      },
-    },
-    {
-      slot: 2,
-      type: {
-        name: "poison",
-        url: "https://pokeapi.co/api/v2/type/4/",
-      },
-    },
-  ],
-};
+// const examplePokemon = {
+//   id: 1,
+//   name: "bulbasaur",
+//   avatar:
+//     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png",
+//   types: [
+//     {
+//       slot: 1,
+//       type: {
+//         name: "grass",
+//         url: "https://pokeapi.co/api/v2/type/12/",
+//       },
+//     },
+//     {
+//       slot: 2,
+//       type: {
+//         name: "poison",
+//         url: "https://pokeapi.co/api/v2/type/4/",
+//       },
+//     },
+//   ],
+// };
 
 export const Favourites = () => {
   return (
     <Page>
       <Title>Favourites</Title>
 
-      <p className="text-white py-6 text-center">
+      {/* <p className="text-white py-6 text-center">
         Here will be list of saved pokemons from localStorage
       </p>
 
@@ -82,28 +82,28 @@ export const Favourites = () => {
           avatar={examplePokemon.avatar}
           number={examplePokemon.id}
         />
-      </div>
+      </div> */}
     </Page>
   );
 }
 
-const PokemonProfile = ({number, name, types, avatar}) => {
+// const PokemonProfile = ({number, name, types, avatar}) => {
 
-return (
-  <figure className="max-w-xs bg-gray-100 rounded-xl p-4">
-    <img
-      className="w-32 h-32 rounded-full mx-auto"
-      src={avatar}
-      alt=""
-      width="384"
-      height="512"
-    />
-    <div className="pt-4 text-center">
-      <figcaption className="font-medium">
-        <div className="text-cyan-600">#{number} {name}</div>
-        <div className="text-gray-500">{types.map(({type}) => type.name).join(', ')}</div>
-      </figcaption>
-    </div>
-  </figure>
-);
-}
+// return (
+//   <figure className="max-w-xs bg-gray-100 rounded-xl p-4">
+//     <img
+//       className="w-32 h-32 rounded-full mx-auto"
+//       src={avatar}
+//       alt=""
+//       width="384"
+//       height="512"
+//     />
+//     <div className="pt-4 text-center">
+//       <figcaption className="font-medium">
+//         <div className="text-cyan-600">#{number} {name}</div>
+//         <div className="text-gray-500">{types.map(({type}) => type.name).join(', ')}</div>
+//       </figcaption>
+//     </div>
+//   </figure>
+// );
+// }
