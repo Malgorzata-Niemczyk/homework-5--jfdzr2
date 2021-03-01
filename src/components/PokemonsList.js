6c31481428031049f8e1b601c7d896385635da43
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-01 22:25:34 +0100

    in the PokemonsList component use different tags and styling classes to display the list of pokemons

--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -16,11 +16,19 @@ const PokemonsList = ({ pokemons, index }) => {
 
     return ( 
         <Link to={`/pokemons/${index + 1}`}>
-            <li className="pokemon-card poke-font hover:bg-red-700 cursor-pointer">
-                <img src={pokemonPic} alt="Pokemon image" />
-                <p>#{index + 1}</p>
-                <p>{pokemons.name}</p>
-            </li>
+            <figure className="pokemon-card max-w-xs bg-gray-100 rounded-xl p-4 poke-font hover:bg-red-700 cursor-pointer">
+                <img
+                    className="w-32 h-32 rounded-full mx-auto"
+                    src={pokemonPic}
+                    alt="Pokemon Avatar"
+                />
+                <div className="pt-4 text-center">
+                    <figcaption className="font-medium">
+                        <div className="text-cyan-600">#{index + 1}</div>
+                        <div className="text-cyan-600">{pokemons.name}</div>
+                    </figcaption>
+                </div>
+            </figure> 
         </Link>
      );
 }
