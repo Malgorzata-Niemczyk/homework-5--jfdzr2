00691bb6e2e24c08dfaea201a45499295992ffdb
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-02 19:13:26 +0100

    take the values of page and totalPages props and output them in a div tag to indicat to a user what page they are on and how many total pages are there; things to be fixed: the increment of the pokemon id when clicking the next button as each time a user is directed to a new set of pokemons it starts from one

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,13 +1,16 @@
 const Pagination = (props) => {
     // console.log(props)
 
-    const { goToNextPage, goToPrevPage } = props;
+    const { goToNextPage, goToPrevPage, page, totalPages } = props;
 
     return (
         <div className="pagination-buttons-wrapper flex poke-font">
           {goToPrevPage && <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={goToPrevPage}>
             Prev
           </button>}
+          <div>
+            {page} of {totalPages}
+          </div>
           {goToNextPage && <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={goToNextPage}>
             Next
           </button>}
