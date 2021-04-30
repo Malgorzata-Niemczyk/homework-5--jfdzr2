9552ad128cae7a54cc61c76a5cf59e1f980d5586
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-04-30 14:21:57 +0200

    add types to the props in page.tsx and title.tsx

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export const Page = ({children, className}) => {
+interface IProps {
+  className: string
+}
+
+
+export const Page: React.FC<IProps> = ({children, className}) => {
   return (
     <div className={`mt-12 p-8 bg-red-500 rounded shadow-xl ${className ? className : ''}`}>
       {children}
